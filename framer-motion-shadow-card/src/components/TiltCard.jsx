17f949b0ad4dbdcc0fbaff1c59f5d93b505c2e5f
ayan878 <ayanraza197@gmail.com>
2024-06-27 01:26:24 +0530

    tilt

--- framer-motion-shadow-card/src/components/TiltCard.jsx
+++ framer-motion-shadow-card/src/components/TiltCard.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function TiltCard() {
   return (
-    <div style={{transform:"translateZ(75px)",
-        transformStyle:"preserve-3d"
-    }} className='absolute inset-4 grid place-content'><div></div></div>
-  )
+    <div
+      style={{
+        transformStyle: "preserve-3d",
+        transform: "rotateY(25deg)",
+      }}
+        // className="relative h-96 w-72 rounded-xl bg-gradient-to-br from-indigo-300 to-violet-300"
+      className="relative h-96 w-72 rounded-xl bg-black"
+    >
+      <div
+        style={{
+          transform: "translateZ(16px) translateY(-8px)",
+          transformStyle: "preserve-3d",
+        }}
+        className="absolute inset-2 grid  rounded-xl bg-yellow-300 shadow-lg  border-2 border-black"
+      >
+        Hover Me
+      </div>
+    </div>
+  );
 }
 
-export default TiltCard
+export default TiltCard;
