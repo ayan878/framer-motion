b7b26df99f9a5ea4167aaa763e179becbcb39ae5
ayan878 <ayanraza197@gmail.com>
2024-06-26 23:59:28 +0530

    app

--- framer-motion-shadow-card/src/App.jsx
+++ framer-motion-shadow-card/src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+
 import ShadowCard from './components/ShadowCards'
 import imgae from "../src/assets/react.svg"
 import Card from './components/Card';
+import TiltCard from './components/TiltCard';
 
 function App() {
   return (
     <div>
-      <ShadowCard
+      {/* <ShadowCard
         imageSrc={imgae}
         altText="Sample Image"
-      />
-      <Card/>
+      /> */}
+      {/* <Card/> */}
+      <TiltCard/>
     </div>
   );
 }
