c9b36e3a2ed9912f0acc828f66b88ed5520eac0b
ayan878 <ayanraza197@gmail.com>
2024-06-26 23:59:14 +0530

    tailwind

--- framer-motion-shadow-card/src/components/TiltCard.jsx
+++ framer-motion-shadow-card/src/components/TiltCard.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function TiltCard() {
   return (
-    <div style={{transform:"translateZ(75px"}}><div></div></div>
+    <div style={{transform:"translateZ(75px)",
+        transformStyle:"preserve-3d"
+    }} className='absolute inset-4 grid place-content'><div></div></div>
   )
 }
 
